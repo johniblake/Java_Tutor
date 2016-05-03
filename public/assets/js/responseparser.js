0b6b87786e973151896d9a86956d7f5ccde3cb48
John Blake <blakej@carleton.edu>
2016-05-03 00:03:25 -0500

    minor fixes to question8

--- public/assets/js/responseparser.js
+++ public/assets/js/responseparser.js
@@ -10,7 +10,7 @@ module.exports = {
     tests["question5"] = "String\\[\\]\\sstringArray\\s=\\snew\\sString\\[10\\];";
     tests["question6"] = "111";
     tests["question7"] = "10";
-    tests["question7"] = "Person's\\sheight\\sis\\s5.";
+    tests["question8"] = "Person's\\sheight\\sis\\s5.";
     return this.isCorrect(response_data, tests);
   },
   isCorrect: function(response_data, tests){
