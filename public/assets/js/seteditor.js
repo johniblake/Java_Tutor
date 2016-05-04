72339faf2ea0e585c64d6f88a08ac523c24b166c
John Blake <blakej@carleton.edu>
2016-05-03 19:28:19 -0500

    checking to see why editor on last page is not loading

--- public/assets/js/seteditor.js
+++ public/assets/js/seteditor.js
@@ -2,6 +2,7 @@ var setEditor = {
 
   init: function (Args) {
     var editor = ace.edit(Args[0]);
+    console.log(Args[1]);
     editor.setTheme("ace/theme/github");
     //editor.setValue(); // or session.setValue
     editor.getSession().setMode("ace/mode/" + Args[1]);
