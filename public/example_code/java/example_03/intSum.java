fb85a7d5d77970a011e534c483df024f46704b58
John Blake <blakej@carleton.edu>
2016-05-06 09:52:37 -0500

    cleaning up code comments/a little spellchecking

--- public/example_code/java/example_03/intSum.java
+++ public/example_code/java/example_03/intSum.java
@@ -3,10 +3,10 @@ public class intSum {
 
     public static void main(String[] args) {
         int a = 1;
-        \\create variable b
-        \\YOUR CODE HERE
-        \\create variable c containing sum of a and b
-        \\YOUR CODE HERE
+        //create variable b
+        //***YOUR CODE HERE***
+        //create variable c containing sum of a and b
+        //***YOUR CODE HERE***
         System.out.println(c);
     }
 }
